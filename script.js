4266b843c85790458d7843f0e9c05bb1121e672e
Eddie <edddyzh@outlook.com>
2025-01-19 21:59:43 +1100

    working in progress, modals updates and working experience

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
+"use strict";
+
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnsCloseModal = document.querySelectorAll(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+const openModal = function (modalId) {
+  document.querySelector(`#${modalId}`).classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnsOpenModal.forEach((button) => {
+  button.addEventListener("click", function () {
+    const modalId = button.dataset.modal; // Fetch the modal ID from the button's data attribute
+    openModal(modalId);
+  });
+});
+
+for (let i = 0; i < btnsCloseModal.length; i++)
+  btnsCloseModal[i].addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  // console.log(e.key);
+
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
